4679ed437bd28894c4702da839a6385bfb779ca2
Yahweh's Delight <intricatesyllable@gmail.com>
2024-09-04 23:48:53 +0200

    Database working

--- app.py
+++ app.py
@@ -1,22 +1,42 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-from flask_wtf import FlaskForm, wtforms
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
-from flask import Flask, render_template, redirect, url_for
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:///database.db'
+
 app.config['SECRET_KEY'] = 'incorrectyoualreadyknow'
 class User(db.model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     username = db.Column(db.String(80), nullable=False)
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+min=4, max=20)], render_kw={"placeholder":"Username"})
+    password = PasswordField(validators=[InputRequired(), Length(
+min=4, max=20)], render_kw={"placeholder":"Password"})
+
+submit = SubmitField("Register")
+
+def validate_username(self, username):
+    existing_user_username = User.query.filterby(username=username.data).first()
+    if existing_user_username:
+        raise ValidationError(
+        "This username already exists, chagua yengine Tafadhali"
+        )
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+min=4, max=20)], render_kw={"placeholder":"Username"})
+    password = PasswordField(validators=[InputRequired(), Length(
+min=4, max=20)], render_kw={"placeholder":"Password"})
+
+submit = SubmitField("Login")
+
 @app.route('/')
 def home():
     return render_template('home.html')
